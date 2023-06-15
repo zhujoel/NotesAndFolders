f47c4ebb50cc5d29f7efc4a2bf5ae420de8194b0
Joel Zhu <joelzhu@outlook.fr>
2023-06-15 19:56:27 +0200

    Use ScalingLazyColumn

--- app/src/main/java/notesandfolders/presentation/navigation/FolderScreen.kt
+++ app/src/main/java/notesandfolders/presentation/navigation/FolderScreen.kt
@@ -5,7 +5,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.wear.compose.material.*
-import notesandfolders.presentation.component.FolderContent
+import notesandfolders.entities.AppDatabase
+import notesandfolders.presentation.component.CreateFolderButton
+import notesandfolders.presentation.component.CreateNoteButton
+import notesandfolders.presentation.component.DeleteFolderButton
+import notesandfolders.presentation.component.FolderCard
+import notesandfolders.presentation.component.NoteCard
 import java.util.UUID
 
 @Composable
@@ -16,14 +21,31 @@ fun FolderScreen(
     folderId: UUID,
     swipeDismissibleNavController: NavHostController
 ) {
+    var folder =  AppDatabase.getDatabase(context).folderDao().get(folderId)
+    var subFolders = AppDatabase.getDatabase(context).folderDao().getChildFolders(folderId).filter { folder -> folder.id != UUID(0L, 0L) }
+    var notes = AppDatabase.getDatabase(context).noteDAO().getChildNotes(folderId)
     val listState = rememberScalingLazyListState()
+
     ScalingLazyColumn(
         modifier = modifier,
         state = listState,
         autoCentering = AutoCenteringParams(itemIndex = 0)
     ) {
-        item {
-            FolderContent(context, modifier, iconModifier, folderId, swipeDismissibleNavController)
-        }
+        item { Text(folder.title) }
+        items(
+            count = subFolders.size,
+            key = { subFolders[it].id },
+            itemContent = {
+                index -> FolderCard(context, modifier, iconModifier, subFolders[index].id, swipeDismissibleNavController)
+            })
+        items(
+            count = notes.size,
+            key = { notes[it].id },
+            itemContent = {
+                    index -> NoteCard(context, modifier, iconModifier, notes[index].id, swipeDismissibleNavController)
+            })
+        item { CreateNoteButton(context, modifier, iconModifier, folderId) }
+        item { CreateFolderButton(context, modifier, iconModifier, folderId) }
+        item { DeleteFolderButton(context, modifier, iconModifier, folder, swipeDismissibleNavController) }
     }
 }
--- app/src/main/java/notesandfolders/presentation/navigation/LandingScreen.kt
+++ app/src/main/java/notesandfolders/presentation/navigation/LandingScreen.kt
@@ -3,11 +3,14 @@ package notesandfolders.presentation.navigation
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.wear.compose.material.*
-import com.example.notesandfolders.R
-import notesandfolders.presentation.component.FolderContent
+import notesandfolders.entities.AppDatabase
+import notesandfolders.presentation.component.CreateFolderButton
+import notesandfolders.presentation.component.CreateNoteButton
+import notesandfolders.presentation.component.DeleteFolderButton
+import notesandfolders.presentation.component.FolderCard
+import notesandfolders.presentation.component.NoteCard
 import java.util.UUID
 
 
@@ -18,16 +21,29 @@ fun LandingScreen(
     swipeDismissibleNavController: NavHostController,
     context: Context
 ) {
-    val listState = rememberScalingLazyListState()
     var rootFolderId = UUID(0L, 0L)
+    var subFolders = AppDatabase.getDatabase(context).folderDao().getChildFolders(rootFolderId)
+    var notes = AppDatabase.getDatabase(context).noteDAO().getChildNotes(rootFolderId)
+    val listState = rememberScalingLazyListState()
 
     ScalingLazyColumn(
         modifier = modifier,
         state = listState,
         autoCentering = AutoCenteringParams(itemIndex = 0)
     ) {
-        item {
-            FolderContent(context, modifier, iconModifier, rootFolderId, swipeDismissibleNavController)
-        }
+        items(
+            count = subFolders.size,
+            key = { subFolders[it].id },
+            itemContent = {
+                    index -> FolderCard(context, modifier, iconModifier, subFolders[index].id, swipeDismissibleNavController)
+            })
+        items(
+            count = notes.size,
+            key = { notes[it].id },
+            itemContent = {
+                    index -> NoteCard(context, modifier, iconModifier, notes[index].id, swipeDismissibleNavController)
+            })
+        item { CreateNoteButton(context, modifier, iconModifier, rootFolderId) }
+        item { CreateFolderButton(context, modifier, iconModifier, rootFolderId) }
     }
 }
